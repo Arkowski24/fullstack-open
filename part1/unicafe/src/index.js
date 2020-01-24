d715835a122ce6eacd2fb1073a0c09100f3e0417
Arkadiusz Placha <arkadiusz.placha@gmail.com>
2020-01-24 23:01:29 +0100

    1.7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import ReactDOM from 'react-dom'
 
 const SectionHeader = (props) => (
@@ -9,15 +9,24 @@ const SectionHeader = (props) => (
 
 const FeedbackValue = (props) => (
     <>
-        <p>{props.buttonName} {props.buttonValue}</p>
+        {props.buttonName} {props.buttonValue}<br/>
     </>
 );
 
 const FeedbackButton = (props) => {
     const incrementButton = () => props.setFun(props.buttonValue + 1);
-    return(
+    return (
         <>
             <button onClick={incrementButton}> {props.buttonName}</button>
+        </>
+    )
+};
+
+const EvaluationField = (props) => {
+    const fieldValue = props.evalFun(props.currentState);
+    return (
+        <>
+            {props.fieldName} {fieldValue}<br/>
         </>
     )
 };
@@ -27,24 +36,32 @@ const App = () => {
     const [neutral, setNeutral] = useState(0);
     const [bad, setBad] = useState(0);
 
+    const currentState = {good: good, neutral: neutral, bad: bad};
+    const evalTotal = (cs) => cs.good + cs.neutral + cs.bad;
+    const evalAverage = (cs) => (cs.good - cs.bad) / evalTotal(cs);
+    const evalPositive = (cs) => cs.good / evalTotal(cs) * 100 + " %";
+
     return (
         <>
             <div>
                 <SectionHeader message={"give feedback"}/>
-                <FeedbackButton buttonName={"Good"} buttonValue={good} setFun={setGood}/>
-                <FeedbackButton buttonName={"Neutral"} buttonValue={neutral} setFun={setNeutral}/>
-                <FeedbackButton buttonName={"Bad"} buttonValue={bad} setFun={setBad}/>
+                <FeedbackButton buttonName={"good"} buttonValue={good} setFun={setGood}/>
+                <FeedbackButton buttonName={"neutral"} buttonValue={neutral} setFun={setNeutral}/>
+                <FeedbackButton buttonName={"bad"} buttonValue={bad} setFun={setBad}/>
             </div>
             <div>
                 <SectionHeader message={"statistics"}/>
-                <FeedbackValue buttonName={"Good"} buttonValue={good}/>
-                <FeedbackValue buttonName={"Neutral"} buttonValue={neutral}/>
-                <FeedbackValue buttonName={"Bad"} buttonValue={bad}/>
+                <FeedbackValue buttonName={"good"} buttonValue={good}/>
+                <FeedbackValue buttonName={"neutral"} buttonValue={neutral}/>
+                <FeedbackValue buttonName={"bad"} buttonValue={bad}/>
+                <EvaluationField fieldName={"sum"} evalFun={evalTotal} currentState={currentState}/>
+                <EvaluationField fieldName={"average"} evalFun={evalAverage} currentState={currentState}/>
+                <EvaluationField fieldName={"positive"} evalFun={evalPositive} currentState={currentState}/>
             </div>
         </>
     )
 };
 
-ReactDOM.render(<App />,
+ReactDOM.render(<App/>,
     document.getElementById('root')
 );
