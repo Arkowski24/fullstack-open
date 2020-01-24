110911a016b88a042c793fec127ee7791c2c02bf
Arkadiusz Placha <arkadiusz.placha@gmail.com>
2020-01-24 23:08:36 +0100

    1.8

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -31,16 +31,30 @@ const EvaluationField = (props) => {
     )
 };
 
+const Statistics = (props) => {
+    const evalTotal = (cs) => cs.good + cs.neutral + cs.bad;
+    const evalAverage = (cs) => (cs.good - cs.bad) / evalTotal(cs);
+    const evalPositive = (cs) => cs.good / evalTotal(cs) * 100 + " %";
+
+    return (
+        <div>
+            <SectionHeader message={"statistics"}/>
+            <FeedbackValue buttonName={"good"} buttonValue={props.currentState.good}/>
+            <FeedbackValue buttonName={"neutral"} buttonValue={props.currentState.neutral}/>
+            <FeedbackValue buttonName={"bad"} buttonValue={props.currentState.bad}/>
+            <EvaluationField fieldName={"sum"} evalFun={evalTotal} currentState={props.currentState}/>
+            <EvaluationField fieldName={"average"} evalFun={evalAverage} currentState={props.currentState}/>
+            <EvaluationField fieldName={"positive"} evalFun={evalPositive} currentState={props.currentState}/>
+        </div>
+    )
+};
+
 const App = () => {
     const [good, setGood] = useState(0);
     const [neutral, setNeutral] = useState(0);
     const [bad, setBad] = useState(0);
 
     const currentState = {good: good, neutral: neutral, bad: bad};
-    const evalTotal = (cs) => cs.good + cs.neutral + cs.bad;
-    const evalAverage = (cs) => (cs.good - cs.bad) / evalTotal(cs);
-    const evalPositive = (cs) => cs.good / evalTotal(cs) * 100 + " %";
-
     return (
         <>
             <div>
@@ -49,15 +63,7 @@ const App = () => {
                 <FeedbackButton buttonName={"neutral"} buttonValue={neutral} setFun={setNeutral}/>
                 <FeedbackButton buttonName={"bad"} buttonValue={bad} setFun={setBad}/>
             </div>
-            <div>
-                <SectionHeader message={"statistics"}/>
-                <FeedbackValue buttonName={"good"} buttonValue={good}/>
-                <FeedbackValue buttonName={"neutral"} buttonValue={neutral}/>
-                <FeedbackValue buttonName={"bad"} buttonValue={bad}/>
-                <EvaluationField fieldName={"sum"} evalFun={evalTotal} currentState={currentState}/>
-                <EvaluationField fieldName={"average"} evalFun={evalAverage} currentState={currentState}/>
-                <EvaluationField fieldName={"positive"} evalFun={evalPositive} currentState={currentState}/>
-            </div>
+            <Statistics currentState={currentState}/>
         </>
     )
 };
