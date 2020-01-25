28f6e90f3ea70a56a967e6332a5288c8494a6a59
Arkadiusz Placha <arkadiusz.placha@gmail.com>
2020-01-25 15:31:33 +0100

    2.5

--- part2/courseContents/src/components/Courses.js
+++ part2/courseContents/src/components/Courses.js
@@ -1,36 +1,5 @@
 import React from 'react'
-
-const Header = ({title}) => <h1>{title}</h1>;
-
-const Content = ({course}) => {
-    const parts = course.parts.map((part) => <Part key={part.id} part={part}/>);
-
-    return (
-        <div>{parts}</div>
-    );
-};
-
-const Part = ({part}) => (
-    <p>
-        {part.name} {part.exercises}
-    </p>
-);
-
-const Total = ({parts}) => {
-    const calculateTotal = (parts) => parts.reduce((a, p) => a + p.exercises, 0);
-
-    return (
-        <b>total of {calculateTotal(parts)} exercises</b>
-    )
-};
-
-const Course = ({course}) => (
-    <div>
-        <Header title={course.name}/>
-        <Content course={course}/>
-        <Total parts={course.parts}/>
-    </div>
-);
+import Course from './Course'
 
 const Courses = ({courses}) => {
     const coursesList = courses.map((course) => <Course course={course}/>);
