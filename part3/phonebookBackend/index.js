58b0d905faa49cb9d530bcf632a4cde91fc54e66
Arkadiusz Placha <arkadiusz.placha@gmail.com>
2020-03-02 11:21:48 +0100

    3.7

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
 let persons =
     [
@@ -27,6 +28,7 @@ let persons =
     ];
 
 app.use(bodyParser.json());
+app.use(morgan('tiny'));
 
 app.get('/info', (request, response) => {
     const date = Date().toString();
