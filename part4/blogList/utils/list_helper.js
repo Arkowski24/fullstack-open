4638dcf60a0f2e2d5f0f73906c342afe33e3d3e2
Arkadiusz Placha <arkadiusz.placha@gmail.com>
2020-03-16 17:37:13 +0100

    4.5

--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -5,7 +5,13 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reducer, 0);
 };
 
+const favoriteBlog = (blogs) => blogs
+  .sort((a, b) => b.likes - a.likes)
+  .slice(0)
+  .map((b) => ({ title: b.title, author: b.author, likes: b.likes }))[0];
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
