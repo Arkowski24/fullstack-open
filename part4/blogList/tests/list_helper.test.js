76e15c47bceefa1eecd32338bda367e9f0baba94
Arkadiusz Placha <arkadiusz.placha@gmail.com>
2020-03-20 21:40:25 +0100

    4.6 & 4.7

--- part4/blogList/tests/list_helper.test.js
+++ part4/blogList/tests/list_helper.test.js
@@ -55,3 +55,37 @@ describe('favourite blog', () => {
     expect(result).toEqual({ title: 'Canonical string reduction', author: 'Edsger W. Dijkstra', likes: 12 });
   });
 });
+
+describe('author with the most blogs', () => {
+  test('of empty list is undefined', () => {
+    const result = listHelper.mostBlogs([]);
+    expect(result).toBe(undefined);
+  });
+
+  test('when list has only one blog equals to one', () => {
+    const result = listHelper.mostBlogs(listWithOneBlog);
+    expect(result).toEqual({ author: 'Edsger W. Dijkstra', blogs: 1 });
+  });
+
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.mostBlogs(blogs);
+    expect(result).toEqual({ author: 'Robert C. Martin', blogs: 3 });
+  });
+});
+
+describe('author with the most likes', () => {
+  test('of empty list is undefined', () => {
+    const result = listHelper.mostLikes([]);
+    expect(result).toBe(undefined);
+  });
+
+  test('when list has only one blog equals to the likes of it', () => {
+    const result = listHelper.mostLikes(listWithOneBlog);
+    expect(result).toEqual({ author: 'Edsger W. Dijkstra', likes: 5 });
+  });
+
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.mostLikes(blogs);
+    expect(result).toEqual({ author: 'Edsger W. Dijkstra', likes: 17 });
+  });
+});
--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -1,3 +1,6 @@
+const _ = require('lodash');
+
+// eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => 1;
 
 const totalLikes = (blogs) => {
@@ -10,8 +13,26 @@ const favoriteBlog = (blogs) => blogs
   .slice(0)
   .map((b) => ({ title: b.title, author: b.author, likes: b.likes }))[0];
 
+const mostBlogs = (blogs) => _.chain(blogs)
+  .countBy((b) => b.author)
+  .toPairs()
+  .map((p) => ({ author: p[0], blogs: p[1] }))
+  .orderBy('blogs')
+  .last()
+  .value();
+
+const mostLikes = (blogs) => _.chain(blogs)
+  .groupBy((b) => b.author)
+  .toPairs()
+  .map((p) => ({ author: p[0], likes: _.sum(p[1].map((b) => b.likes)) }))
+  .orderBy('likes')
+  .last()
+  .value();
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
+  mostLikes,
 };
