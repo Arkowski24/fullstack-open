10c4077cf08f47b8120b4641efcf70f2708cd4e0
Arkadiusz Placha <arkadiusz.placha@gmail.com>
2020-05-03 21:06:29 +0200

    5.7

--- part5/blogListFrontend/src/components/Blog.js
+++ part5/blogListFrontend/src/components/Blog.js
@@ -1,8 +1,47 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react';
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [hidden, setHidden] = useState(true);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  };
+
+  const handleTitleButtonClick = (e) => {
+    e.preventDefault();
+    setHidden(!hidden);
+  };
+
+  const title = () => (
+    <div>
+      {blog.title + ' '}
+      <button type='button' onClick={handleTitleButtonClick}>
+        {hidden ? 'view' : 'hide'}
+      </button>
+    </div>
+  );
+
+  const details = () => (
+    <div>
+      <div>{blog.url}</div>
+      <div>
+        {`likes ${blog.likes} `}
+        <button type='button'>like</button>
+      </div>
+      <div>{blog.author}</div>
+    </div>
+  );
+
+  return (
+    <div style={blogStyle}>
+      {title()}
+      {hidden || details()}
+    </div>
+  );
+};
+
+export default Blog;
