126e8270f687933191f0f1765f4af035e398621a
Arkadiusz Placha <arkadiusz.placha@gmail.com>
2020-05-07 12:01:53 +0200

    5.14

--- part5/blogListFrontend/src/components/Blog.test.js
+++ part5/blogListFrontend/src/components/Blog.test.js
@@ -1,24 +1,49 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 import Blog from './Blog';
 
 
-test('<Blog /> displays only title and author at first', () => {
+describe('<Blog />', () => {
   const blog = {
-    _id: '5a422a851b54a676234d17f7', title: 'React patterns', author: 'Michael Chan', url: 'https://reactpatterns.com/', likes: 7, __v: 0,
+    _id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    user: {
+      _id: '7f71d432676a45b158a224a5',
+      name: 'Example User'
+    },
+    likes: 7,
+    __v: 0,
   };
-  const modifyBlog = jest.fn();
-  const deleteBlog = jest.fn();
-  const isRemovable = true;
+  let component;
 
-  const component = render(
-    <Blog blog={blog} modifyBlog={modifyBlog} deleteBlog={deleteBlog} isRemovable={isRemovable}/>
-  );
+  beforeEach(() => {
+    const modifyBlog = jest.fn();
+    const deleteBlog = jest.fn();
+    const isRemovable = true;
 
-  expect(component.container).toHaveTextContent(blog.title);
-  expect(component.container).toHaveTextContent(blog.author);
-  expect(component.container).not.toHaveTextContent(blog.url);
-  expect(component.container).not.toHaveTextContent(blog.likes);
+    component = render(
+      <Blog blog={blog} modifyBlog={modifyBlog} deleteBlog={deleteBlog} isRemovable={isRemovable}/>
+    );
+  });
+
+  test('displays only title and author at first', () => {
+    expect(component.container).toHaveTextContent(blog.title);
+    expect(component.container).toHaveTextContent(blog.author);
+    expect(component.container).not.toHaveTextContent(blog.url);
+    expect(component.container).not.toHaveTextContent(blog.likes);
+  });
+
+  test(' displays all information after click', () => {
+    const button = component.getByText('view');
+    fireEvent.click(button);
+
+    expect(component.container).toHaveTextContent(blog.title);
+    expect(component.container).toHaveTextContent(blog.author);
+    expect(component.container).toHaveTextContent(blog.url);
+    expect(component.container).toHaveTextContent(blog.likes);
+  });
 });
