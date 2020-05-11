d52a9751bb62ee3e570486bf039beed7d644a79b
Arkadiusz Placha <arkadiusz.placha@gmail.com>
2020-05-11 12:11:42 +0200

    5.18

--- part5/blogListFrontend/cypress/integration/blog_app.spec.js
+++ part5/blogListFrontend/cypress/integration/blog_app.spec.js
@@ -1,6 +1,15 @@
 describe('Blog app', function() {
+  const user = {
+    username: 'Username1',
+    name: 'TestUser',
+    password: '1234'
+  };
+
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3001/api/testing/reset');
+    cy.clearLocalStorage();
+    cy.request('POST', 'http://localhost:3001/api/users/', user);
+
     cy.visit('http://localhost:3000');
   });
 
@@ -11,4 +20,25 @@ describe('Blog app', function() {
     cy.contains('login');
   });
 
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type(user.username);
+      cy.get('#password').type(user.password);
+      cy.get('#loginFormButton').click();
+
+      cy.contains('Logged in')
+        .should('be.visible')
+        .and('have.css', 'border-color', 'rgb(0, 128, 0)');
+    });
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type(user.username);
+      cy.get('#password').type(user.password + '1234');
+      cy.get('#loginFormButton').click();
+
+      cy.contains('Invalid username or password')
+        .should('be.visible')
+        .and('have.css', 'border-color', 'rgb(255, 0, 0)');
+    });
+  });
 });
--- part5/blogListFrontend/src/services/blogs.js
+++ part5/blogListFrontend/src/services/blogs.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3003/api/blogs';
+const baseUrl = 'http://localhost:3001/api/blogs';
 
 let token = null;
 
