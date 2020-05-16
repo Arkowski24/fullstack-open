f5717391c45705f2786cef0cd803696860a8e494
Arkadiusz Placha <arkadiusz.placha@gmail.com>
2020-05-16 14:57:45 +0200

    6.13

--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
 import App from './App';
+import { initAnecdotes } from './reducers/anecdoteReducer';
+import anecdotesService from './services/anecdotes';
 import store from './store';
+
+anecdotesService.getAll()
+  .then(anecdotes => store.dispatch(initAnecdotes(anecdotes)));
 
 ReactDOM.render(
   <Provider store={store}>
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,12 +1,3 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-];
-
 const getId = () => (100000 * Math.random()).toFixed(0);
 
 const asObject = (anecdote) => {
@@ -17,22 +8,22 @@ const asObject = (anecdote) => {
   };
 };
 
-const initialState = anecdotesAtStart.map(asObject);
-
-const reducer = (state = initialState, action) => {
+const reducer = (state = [], action) => {
   console.log('state now: ', state);
   console.log('action', action);
 
   switch (action.type) {
-    case 'VOTE':
+    case 'ANECDOTE_VOTE':
       const anecdote = state.find(a => a.id === action.data.id);
       const newAnecdote = {
         ...anecdote,
         votes: anecdote.votes + 1
       };
       return state.map(a => a.id === action.data.id ? newAnecdote : a);
-    case 'ADD':
+    case 'ANECDOTE_ADD':
       return state.concat(asObject(action.data.content));
+    case 'ANECDOTE_INIT':
+      return state.concat(action.data);
     default:
       return state;
   }
@@ -40,15 +31,22 @@ const reducer = (state = initialState, action) => {
 
 export const voteAnecdote = (id) => {
   return {
-    type: 'VOTE',
+    type: 'ANECDOTE_VOTE',
     data: { id }
   };
 };
 
 export const addAnecdote = (content) => {
   return {
-    type: 'ADD',
+    type: 'ANECDOTE_ADD',
     data: { content }
+  };
+};
+
+export const initAnecdotes = (data) => {
+  return {
+    type: 'ANECDOTE_INIT',
+    data
   };
 };
 
