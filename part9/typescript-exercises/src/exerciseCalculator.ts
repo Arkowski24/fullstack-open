71c50a5521e35bed3d2cf6c6c339d45b95818c36
Arkadiusz Placha <arkadiusz.placha@gmail.com>
2020-06-04 01:34:50 +0200

    9.3

--- part9/typescript-exercises/src/exerciseCalculator.ts
+++ part9/typescript-exercises/src/exerciseCalculator.ts
@@ -1,3 +1,20 @@
+interface ExercisesInput {
+    dailyExerciseHours: Array<number>,
+    targetDailyHours: number
+}
+
+const parseExerciseArguments = (args: Array<string>): ExercisesInput => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+    args = args.slice(2);
+    const numbers = args.map(a => Number(a));
+    const nans = numbers.filter(n => isNaN(n) || !isFinite(n));
+    if (nans.length > 0) throw new Error('Provided non-finite number');
+    return {
+        dailyExerciseHours: numbers.slice(1),
+        targetDailyHours: numbers[0]
+    }
+};
+
 interface Result {
     periodLength: number,
     trainingDays: number,
@@ -8,16 +25,16 @@ interface Result {
     average: number
 }
 
-const calculateExercises = (dailyExerciseHours: number[], targetDailyHours: number): Result => {
+const calculateExercises = (dailyExerciseHours: Array<number>, targetDailyHours: number): Result => {
     const periodLength = dailyExerciseHours.length;
     const trainingDays = dailyExerciseHours.filter(h => h > 0).length;
     const successDays = dailyExerciseHours.filter(h => h >= targetDailyHours).length;
     const success = successDays === periodLength;
-    const rating = success ? 3 : (successDays / periodLength > 0.5 ? 2 : 1);
+    const average = dailyExerciseHours.reduce((a, b) => a + b, 0) / periodLength;
+    const rating = success ? 3 : (average / targetDailyHours >= 0.5 ? 2 : 1);
     const ratingDescription = rating === 3 ? 'congratulations'
         : (rating === 2 ? 'not too bad but could be better' : 'you have to try a bit harder');
     const target = targetDailyHours;
-    const average = dailyExerciseHours.reduce((a, b) => a + b, 0) / periodLength;
     return {
         periodLength,
         trainingDays,
@@ -29,4 +46,10 @@ const calculateExercises = (dailyExerciseHours: number[], targetDailyHours: numb
     };
 };
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
+try {
+    const {dailyExerciseHours, targetDailyHours} = parseExerciseArguments(process.argv);
+    const result = calculateExercises(dailyExerciseHours, targetDailyHours);
+    console.log(result);
+} catch (e) {
+    console.log('Error: ', e.message);
+}
